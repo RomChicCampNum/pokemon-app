90a816277dc93a8e74be47d597a5c2bc234e3e12
Romain <romain.chica@le-campus-numerique.com>
2024-10-30 12:21:45 +0100

    directive done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
-  `,
-  styles: []
+  templateUrl:'app.component.html'
 })
-export class AppComponent {
-  title = 'pokemon-app';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+  ngOnInit() {
+    console.table (this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const id = +pokemonId;
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon) {
+      console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokemon qui n'existe pas.`)
+      this.pokemonSelected = pokemon;
+    }
+    
+  }
 }
+ 
